e6e782aad4cb16c1c14fb7851521ecae124dfd54
Ankush Patel <ankush14.dev@gmail.com>
2020-04-06 22:48:58 +0530

    added try-except

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ from redis import Redis
 app = Flask(__name__)
 api = Api(app)
 
-client = Redis(host='redis', port=6379)
 
 
 class Visits(Resource):
 
     def get(self):
         try:
+            client = Redis(host='redis', port=6379)
             visits = client.get('visits')
             if visits == None:
                 visits = 0
@@ -19,7 +19,10 @@ class Visits(Resource):
             visits = 0
         visits = int(visits)
         visits += 1
-        client.set('visits', visits)
+        try:
+            client.set('visits', visits)
+        except:
+            pass
         return {'visits': visits}
 
 api.add_resource(Visits, "/")
